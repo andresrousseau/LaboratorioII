73ed7cc0b3d1b4953c71862198cba841db0dec1f
andresrousseau <53940793+andresrousseau@users.noreply.github.com>
2019-08-15 20:05:09 -0300

    Trabajo Practico N2 Ej2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,25 +8,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int [] arrNumeros = new int [10];
-        int pos = 0;
-        int neg = 0;
-        int cer = 0;
+        int media = 0;
+        int pares = 0;
 
         for (int i = 0; i <arrNumeros.length ; i++) {
             System.out.println("Ingres 10 numeros enteros: ");
             arrNumeros [i] = sc.nextInt();
 
-
         }
-       for (int valor:arrNumeros){
-           if (valor > 0){
-               pos++;
-           }else
-           if (valor < 0){
-               neg++;
-           }else
-               cer++;
-       }
-        System.out.println("Positivos: "+pos+" Negativos: "+neg+" Ceros: "+cer);
+        for (int i = 0; i < arrNumeros.length; i++) {
+            if (i%2 == 0 && i != 0){
+                pares++;
+                media = media + arrNumeros[i];
+            }
+        }
+        System.out.println("La media de las posiciones pares son: "+media/pares);
     }
 }

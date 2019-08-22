7ce4066034e7a09ebb8cdce4821e328d53d2a4c3
andresrousseau <53940793+andresrousseau@users.noreply.github.com>
2019-08-22 00:44:56 -0300

    Trabajo Practico Nº 3 E3

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,5 +21,31 @@ public class Main {
             }
             System.out.println(" ");
         }
+        for (int i = 0; i < matriz.length ; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                for (int k = 0; k < matriz.length ; k++) {
+                    for (int l = 0; l < matriz.length; l++) {
+
+                        if (matriz[i][j] < matriz[k][l]){
+                            int t = matriz[i][j];
+                            matriz [i][j] = matriz [k][l];
+                            matriz[k][l] = t;
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+        System.out.println("- - - - - - -");
+        for (int[] dimension1:matriz){
+            for (int elemento:dimension1){
+                System.out.print(elemento+" ");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
